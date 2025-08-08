38ca8e512ed07bd24191ac2d4a3336c3cd2296f2
ahmadkamran710788 <ahmadkamran710788@gmail.com>
2025-08-08 16:39:26 +0500

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,9 +20,9 @@ const Header = () => {
             <div className="contact-info">
               <span>Need Assistance? Please Call: +92-309-5555213</span>
             </div>
-            <div className="cta-button">
+            {/* <div className="cta-button">
               <button className="get-offer-btn">Get Offer</button>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
